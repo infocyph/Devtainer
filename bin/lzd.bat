45f6a02ec634faebb340b73ddf3556363a42d81d
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2025-05-04 14:28:54 +0600

    executables

--- bin/lzd.bat
+++ bin/lzd.bat
@@ -1,3 +1,15 @@
 @echo off
+setlocal
 
-docker exec -it SERVER_TOOLS lazydocker
+set "SERVICE=SERVER_TOOLS"
+
+rem verify container is running
+docker ps --format "{{.Names}}" | findstr /ix "%SERVICE%" >nul 2>&1
+if errorlevel 1 (
+  echo Error: container "%SERVICE%" is not running.
+  endlocal & exit /b 1
+)
+
+rem exec lazydocker
+docker exec -it "%SERVICE%" lazydocker
+endlocal
